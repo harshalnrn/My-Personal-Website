91e14bf7c7d439c84a459eb4133b332ed8f68b61
Harshal <harshalnrn>
2020-11-26 16:55:25 +0530

    first version of my personal website

---
+++ js/script.js
@@ -0,0 +1,107 @@
+/* var link=document.getElementById('toggleCardDescription1');
+link.addEventListener('click',toggleCardBody1);
+
+
+
+var link=document.getElementById('toggleCardDescription2');
+link.addEventListener('click',toggleCardBody2);
+
+var link=document.getElementById('toggleCardDescription3');
+link.addEventListener('click',toggleCardBody3);
+
+var link=document.getElementById('toggleCardDescription4');
+link.addEventListener('click',toggleCardBody4);
+
+var link=document.getElementById('toggleCardDescription5');
+link.addEventListener('click',toggleCardBody5);
+
+var link=document.getElementById('toggleCardDescription6');
+link.addEventListener('click',toggleCardBody6);
+
+ 
+function toggleCardBody1(){
+if(document.getElementsByClassName('card-toggle-container1')[0].style.display==='none'){
+document.getElementsByClassName('card-toggle-container1')[0].style.display='block'
+document.getElementById('toggleCardDescription1').className='fa fa-sort-asc'
+}
+else{
+document.getElementsByClassName('card-toggle-container1')[0].style.display='none'
+document.getElementById('toggleCardDescription1').className='fa fa-sort-desc'
+}
+
+
+}
+
+function toggleCardBody2(){
+
+
+if(document.getElementsByClassName('card-toggle-container2')[0].style.display==='none'){
+document.getElementsByClassName('card-toggle-container2')[0].style.display='block'
+document.getElementById('toggleCardDescription2').className='fa fa-sort-asc'
+}
+else{
+document.getElementsByClassName('card-toggle-container2')[0].style.display='none'
+document.getElementById('toggleCardDescription2').className='fa fa-sort-desc'
+}
+
+}
+
+
+function toggleCardBody3(){
+
+
+if(document.getElementsByClassName('card-toggle-container3')[0].style.display==='none'){
+document.getElementsByClassName('card-toggle-container3')[0].style.display='block'
+document.getElementById('toggleCardDescription3').className='fa fa-sort-asc'
+}
+else{
+document.getElementsByClassName('card-toggle-container3')[0].style.display='none'
+document.getElementById('toggleCardDescription3').className='fa fa-sort-desc'
+}
+
+}
+
+
+function toggleCardBody4(){
+
+
+if(document.getElementsByClassName('card-toggle-container4')[0].style.display==='none'){
+document.getElementsByClassName('card-toggle-container4')[0].style.display='block'
+document.getElementById('toggleCardDescription4').className='fa fa-sort-asc'
+}
+else{
+document.getElementsByClassName('card-toggle-container4')[0].style.display='none'
+document.getElementById('toggleCardDescription4').className='fa fa-sort-desc'
+}
+
+}
+
+
+function toggleCardBody5(){
+
+
+if(document.getElementsByClassName('card-toggle-container5')[0].style.display==='none'){
+document.getElementsByClassName('card-toggle-container5')[0].style.display='block'
+document.getElementById('toggleCardDescription5').className='fa fa-sort-asc'
+}
+else{
+document.getElementsByClassName('card-toggle-container5')[0].style.display='none'
+document.getElementById('toggleCardDescription5').className='fa fa-sort-desc'
+}
+
+}
+
+
+function toggleCardBody6(){
+
+
+if(document.getElementsByClassName('card-toggle-container6')[0].style.display==='none'){
+document.getElementsByClassName('card-toggle-container6')[0].style.display='block'
+document.getElementById('toggleCardDescription6').className='fa fa-sort-asc'
+}
+else{
+document.getElementsByClassName('card-toggle-container6')[0].style.display='none'
+document.getElementById('toggleCardDescription6').className='fa fa-sort-desc'
+}
+
+} */
